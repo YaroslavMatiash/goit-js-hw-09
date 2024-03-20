523a17127b9bdf53d5b4b77924c0b4cae66851de
YaroslavMatiash <150804385+YaroslavMatiash@users.noreply.github.com>
2024-03-20 10:09:09 +0000

    Deploying to gh-pages from @ YaroslavMatiash/goit-js-hw-09@3f8028d05c144d8c3b30bed38899cf217c8719c0 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */console.log("Gallery");
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{S as o}from"./assets/vendor-870f0eb5.js";new o(".gallery a",{});console.log(o);
 //# sourceMappingURL=commonHelpers.js.map
